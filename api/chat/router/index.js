456869bd8924b5a501da24e75160ac30f2beaa0b
Takumi Masuda <takumi.mas@hotmail.com>
2023-03-14 13:34:14 +0900

    chat取得

--- api/chat/router/index.js
+++ api/chat/router/index.js
@@ -143,4 +143,97 @@ router.get("/room",async (req,res)=>{
     }
 })
 
+/*
+    チャットデータ取得
+    リクエストクエリ：
+    {
+        id: string（roomId）
+    }
+    レスポンス：
+    [
+        {
+            text: string
+            createdTs: number
+            userId: string
+            userName: string
+        }
+    ]
+*/
+router.get("/room/chat",async (req,res)=>{
+    const authResp = await auth(req)
+    if (authResp.status) {
+        const getRoomIndex = {
+            TableName: dbname["Room"],
+            Key:{
+                id: req.query["id"]
+            }
+        }
+        const resp = documentClient.get(getRoomIndex).promise()
+        const queryMemberReq = {
+            TableName: dbname["Member"],
+            IndexName: "userId-index",
+            ExpressionAttributeValues:{
+                ":userId":authResp.user["cognito:username"]
+            },
+            ExpressionAttributeNames:{
+                "#userId":"userId"
+            },
+            KeyConditionExpression:"#userId = :userId"
+        }
+        const memberResp = await documentClient.query(queryMemberReq).promise()
+        if (memberResp.Count === 0) {
+            res.status(401).json({
+                status:false
+            })
+            return
+        }
+        let access = false
+        memberResp.Items.forEach(item=>{
+            if(item.organizationId === resp.Item.organizationId) {
+                access = true
+            }
+        })
+        if (!access) {
+            res.status(401).json({
+                status:false
+            })
+            return
+        }
+        const queryChatInput = {
+            TableName: dbname["Chat"],
+            IndexName: "roomId-createdTs-index",
+            ExpressionAttributeNames:{
+                "#roomId":"roomId"
+            },
+            ExpressionAttributeValues:{
+                ":roomId":req.query["id"]
+            },
+            KeyConditionExpression:"#roomId = :roomId"
+        }
+        const chatResp = documentClient.query(queryChatInput).promise()
+        const responseData = []
+
+        await Promise.all(chatResp.Items.map(async(item)=>{
+            const getPostUserInput = {
+                TableName: dbname["User"],
+                Key: {
+                    id: item.userId
+                }
+            }
+            const getPostUserResp = documentClient.get(getPostUserInput).promise()
+            responseData.push({
+                text: item.text,
+                createdTs: item.createdTs,
+                userId: item.userId,
+                userName: getPostUserResp.Item.name
+            })
+        }))
+        res.json(responseData)
+    } else {
+        res.status(401).json({
+            status:false
+        })
+    }
+})
+
 module.exports = router
