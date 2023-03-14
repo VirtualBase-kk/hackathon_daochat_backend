ac4aadfb3001edb2b617216619e75a2944930809
Takumi Masuda <takumi.mas@hotmail.com>
2023-03-14 11:31:59 +0900

    todo完了

--- api/todo/router/index.js
+++ api/todo/router/index.js
@@ -72,4 +72,79 @@ router.post("/todo/create",async(req,res)=>{
     }
 } )
 
+/*
+    Todoを完了
+    リクエスト：
+    {
+        id: string
+    }
+    レスポンス：
+    {
+        status: bool
+    }
+*/
+router.post("/todo/done",async(req,res)=>{
+    const authResp = await auth(req)
+    if (authResp.status) {
+        const getTodoReq = {
+            TableName: dbname["TodoItem"],
+            Key:{
+                id:req.body["id"]
+            }
+        }
+        const todoResp = await documentClient.get(getTodoReq).promise()
+        const queryMemberReq = {
+            TableName: dbname["Member"],
+            IndexName: "userId-index",
+            ExpressionAttributeValues:{
+                ":userId":authResp.user["cognito:username"]
+            },
+            ExpressionAttributeNames:{
+                "#userId":"userId"
+            },
+            KeyConditionExpression:"#userId = :userId"
+        }
+        const memberResp = await documentClient.query(queryMemberReq).promise()
+        if (memberResp.Count === 0) {
+            res.status(401).json({
+                status:false
+            })
+            return
+        }
+        let access = false
+        memberResp.Items.forEach(item=>{
+            if(item.organizationId === todoResp.Item["organizationId"]) {
+                access = true
+            }
+        })
+        if (!access) {
+            res.status(401).json({
+                status:false
+            })
+            return
+        }
+        const updateTodoItem = {
+            TableName: dbname["TodoItem"],
+            Key:{
+                id:req.body["id"]
+            },
+            ExpressionAttributeNames:{
+                "#status":"status",
+            },
+            ExpressionAttributeValues:{
+                ":status":true
+            },
+            UpdateExpression: "set #status = :status"
+        }
+        await documentClient.update(updateTodoItem).promise()
+        res.json({
+            status:true
+        })
+    } else {
+        res.status(401).json({
+            status:false
+        })
+    }
+} )
+
 module.exports = router
