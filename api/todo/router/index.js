5c9c6d9c6229cc408168a2aed78c7817640def6b
Takumi Masuda <takumi.mas@hotmail.com>
2023-03-14 11:24:53 +0900

    todo作成

--- api/todo/router/index.js
+++ api/todo/router/index.js
@@ -24,7 +24,47 @@ const Web3 = require("web3")
 router.post("/todo/create",async(req,res)=>{
     const authResp = await auth(req)
     if (authResp.status) {
-
+        const queryMemberReq = {
+            TableName: dbname["Member"],
+            IndexName: "userId-index",
+            ExpressionAttributeValues:{
+                ":userId":authResp.user["cognito:username"]
+            },
+            ExpressionAttributeNames:{
+                "#userId":"userId"
+            },
+            KeyConditionExpression:"#userId = :userId"
+        }
+        const memberResp = await documentClient.query(queryMemberReq).promise()
+        if (memberResp.Count === 0) {
+            res.status(401).json({
+                status:false
+            })
+            return
+        }
+        let access = false
+        memberResp.Items.forEach(item=>{
+            if(item.organizationId === req.body["organization"]) {
+                access = true
+            }
+        })
+        if (!access) {
+            res.status(401).json({
+                status:false
+            })
+            return
+        }
+        const id = uuidv4()
+        const putTodoItem = {
+            TableName: dbname["TodoItem"],
+            Item:{
+                id: id,
+                title: req.body["name"],
+                status: false,
+                organization: req.body["organization"]
+            }
+        }
+        await documentClient.put(putTodoItem).promise()
     } else {
         res.status(401).json({
             status:false
